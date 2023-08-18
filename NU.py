35b8112e7117b9eaafe96506e1cf8f474d7768c5
Tanya-Polyakova <141919019+Tanya-Polyakova@users.noreply.github.com>
2023-08-18 17:39:13 +0300

    17.39

--- NU.py
+++ NU.py
@@ -16,24 +16,24 @@ def get_text_messages(message):
 
     if message.text == 'Проверить страницу':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Животные, части тел животных и продукты из них')
+        btn1 = types.KeyboardButton('Животные')
         btn2 = types.KeyboardButton('18+')
-        btn3 = types.KeyboardButton('Казино и азартные игры')
-        btn4 = types.KeyboardButton('Сигареты и табачная продукция')
-        btn5 = types.KeyboardButton('Контрафактная и пиратская продукция')
-        btn6 = types.KeyboardButton('Лекарственные и наркотические средства и аксессуары для их употребления')
-        btn7 = types.KeyboardButton('Полицейское и военное снаряжение')
-        btn8 = types.KeyboardButton('Политическая и социальная реклама')
-        btn9 = types.KeyboardButton('Неприемлемые методы ведения бизнеса')
-        btn10 = types.KeyboardButton('Услуги по прерыванию беременности')
+        btn3 = types.KeyboardButton('Казино')
+        btn4 = types.KeyboardButton('Сигареты')
+        btn5 = types.KeyboardButton('Пиратская продукция')
+        btn6 = types.KeyboardButton('Наркотики')
+        btn7 = types.KeyboardButton('Военное снаряжение')
+        btn8 = types.KeyboardButton('Социальная реклама')
+        btn9 = types.KeyboardButton('Неприемлемые методы')
+        btn10 = types.KeyboardButton('Беременность')
         btn11 = types.KeyboardButton('Ритуальные услуги')
-        btn12 = types.KeyboardButton('Опасные химические вещества')
-        btn13 = types.KeyboardButton('Торговля человеческими органами')
+        btn12 = types.KeyboardButton('Химические вещества')
+        btn13 = types.KeyboardButton('Человеческие органы')
         btn14 = types.KeyboardButton('Кастинг моделей')
-        btn15 = types.KeyboardButton('Определение пола ребенка до рождения')
+        btn15 = types.KeyboardButton('Пол ребенка')
         btn16 = types.KeyboardButton('Спорная деятельность')
-        btn17 = types.KeyboardButton('Оружие и его части')
-        btn18 = types.KeyboardButton('Эзотерика и магия')
+        btn17 = types.KeyboardButton('Оружие')
+        btn18 = types.KeyboardButton('Эзотерика')
         btn19 = types.KeyboardButton('Лекарства')
         btn20 = types.KeyboardButton('Медицина')
         btn21 = types.KeyboardButton('БАД')
@@ -59,15 +59,30 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Выберите тематику', reply_markup=markup) #ответ бота
 
 
-    elif message.text == 'Животные, части тел животных и продукты из них':
+    elif message.text == 'Животные':
         bot.send_message(message.from_user.id, 'Продвижение, продажа или предложение животных, включая живых животных, домашний скот, домашних животных, туши или продукты животного происхождения или их части, а также содействие в доступе к ним.', parse_mode='Markdown')
 
     elif message.text == '18+':
         bot.send_message(message.from_user.id, 'Продвижение, продажа, предложение порнографических материалов, секс-игрушек и расходных материалов к ним, например смазок, эротических костюмов (фетиш-костюмы или костюмы для сексуальных фантазий), а также содействие в доступе к ним.', parse_mode='Markdown')
 
-    elif message.text == 'Азартные игры':
+    elif message.text == 'Казино':
         bot.send_message(message.from_user.id, 'Продвижение, продажа, предложение услуг казино, азартных игр, размещения и приема ставок на спортивные события, услуг фэнтези-спорта, лотерей (в виде веб-сайта или физического учреждения, в котором доступны услуги монетизации и обналичивания денег), а также содействие в доступе к ним.', parse_mode='Markdown')
 
+
+@bot.message_handler(commands=['step1'])
+def start(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Требования к целевой страницы")
+    markup.add(btn2)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
+        btn1 = types.KeyboardButton('Конфиденциальность')
+        btn2 = types.KeyboardButton('Тест1')
+        btn3 = types.KeyboardButton('Тест2')
 
 bot.polling(none_stop=True) #обязательная для работы бота часть
 
